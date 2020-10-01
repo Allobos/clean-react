faada14c64d9ddb0294298b7d4de7d5df970574b
Jonnie <jonnieallobos@hotmail.com>
2020-10-01 00:20:56 -0300

    refactor: add generic params to HttpPostClient

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,9 +1,12 @@
 import { HttpResponse } from './http-response'
-export type HttpPostParams = {
-  url: string
-  body?: object
-}
 
-export interface HttpPostClient {
-  post (params: HttpPostParams): Promise<HttpResponse>
+export type HttpPostParams<T> = {
+  url: string
+  body?: T
+}
+// Tipos genéricos do Typescript
+// T tipo do body do parâmetro
+// R tipo do body da função
+export interface HttpPostClient<T, R> {
+  post (params: HttpPostParams<T>): Promise<HttpResponse<R>>
 }
