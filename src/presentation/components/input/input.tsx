b69d54b5a974c8dd785903fa97e5ebebbefec6e0
Jonnie <jonnieallobos@hotmail.com>
2020-10-05 21:46:04 -0300

    feat: ensure Login start with inputs with required state

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,14 +1,23 @@
 // não utilizamos memo porque este componente fica mudando de estado
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './input-styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
 
-// Props = guarda as propriedades que essa classe vai receber, esse nosso props aqui é igual ao props do <input type="email"> que está comentado dentro do return mais abaixo - é só deixar o mouse estacionado sobre o <input> para ver as propriedades. Com isso, não precisamos digitar todo o textão todas as vezes que formos utilizar esse input, é só chamar o props no local onde o <input> estava no código que será entendido todas as propriedades do input!
+// Props guarda as propriedades do <input type="email"> que está comentado dentro do return mais abaixo
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const { errorState } = useContext(Context)
+  const error = errorState[props.name]
   // Hackzinho para bloquear o autocomplete do Chrome capturando o evento do onFocus do input
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+  const getStatus = (): string => {
+    return 'O'
+  }
+  const getTitle = (): string => {
+    return error
   }
 
   return (
@@ -19,9 +28,11 @@ const Input: React.FC<Props> = (props: Props) => {
           e atribuir o hackzinho no onFocus do input
       */}
       <input {...props} readOnly onFocus={enableInput} />
-      <span className={Styles.status}></span>
+      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
 }
 
 export default Input
+
+// é só deixar o mouse estacionado sobre o <input> para ver as propriedades. Com isso, não precisamos digitar todo o textão todas as vezes que formos utilizar esse input, é só chamar o props no local onde o <input> estava no código que será entendido todas as propriedades do input!
