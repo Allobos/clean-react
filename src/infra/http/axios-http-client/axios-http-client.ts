8f5a4c58330c5c416edfc895b6d58c8a5063fd10
Jonnie <jonnieallobos@hotmail.com>
2020-10-24 22:27:02 -0300

    fix: change AxiosHttpClient to handle exceptions

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,11 +1,16 @@
 import { HttpPostParams, HttpResponse, HttpPostClient } from '@/data/protocols/http'
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 // design pattern Adapter - estamos adaptando duas interfaces diferentes:
 // uma interface é HttpPostclient<T, R> e a outra é Axios
 export class AxiosHttpClient implements HttpPostClient<any, any> {
   async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
-    const httpResponse = await axios.post(params.url, params.body)
+    let httpResponse: AxiosResponse<any>
+    try {
+      httpResponse = await axios.post(params.url, params.body)
+    } catch (error) {
+      httpResponse = error.response
+    }
     return {
       statusCode: httpResponse.status,
       body: httpResponse.data
