3d4f8b014f329816d7f8d1094a76a4d5725250a4
Jonnie <jonnieallobos@hotmail.com>
2020-10-26 12:54:23 -0300

    test: ensure SignUp change name status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,31 +1,55 @@
 import React from 'react'
+import faker from 'faker'
+import { RenderResult, render, cleanup, fireEvent } from '@testing-library/react'
 import SignUp from './signup'
-import { RenderResult, render } from '@testing-library/react'
-import { Helper } from '@/presentation/test'
+import { Helper, ValidationStub } from '@/presentation/test'
+
 type SutTypes = {
   sut: RenderResult
 }
 
-const makeSut = (): SutTypes => {
+type SutParams = {
+  validationError: string
+}
+
+const makeSut = (params?: SutParams): SutTypes => {
+  const validationStub = new ValidationStub()
+  validationStub.errorMessage = params?.validationError // se não for passado o valor será nulo indicando que não tem erro
   const sut = render(
-    <SignUp />
+    <SignUp
+      // validationStub é uma versão mockada, de uma dependência nossa, que valida se os campos são válidos ou não
+      validation={validationStub}
+    />
   )
   return {
     sut
   }
 }
 
+const populateField = (sut: RenderResult, fieldName: string, value = faker.random.word()): void => {
+  const input = sut.getByTestId(fieldName)
+  fireEvent.input(input, { target: { value } }) // popula o input do campo
+}
+
 describe('SignUp Component', () => {
+  afterEach(cleanup) // garante que qualquer alteração no render(<Login />) de um teste não afete o teste de baixo
   // testa o estado inicial dos campos
   test('Should start with initial state', () => {
-    const validationError = 'Campo obrigatório'
-    const { sut } = makeSut()
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
     Helper.testChildCount(sut, 'error-wrap', 0) // testa que o error-wrap não pode ter nenhum filho
     Helper.testButtonIsDisabled(sut, 'submit', true) // que o botão de submit tem que estar desbilitado
     // testa todos os campos com status inicial 'Campo obrigatório'
     Helper.testStatusForField(sut, 'name', validationError)
-    Helper.testStatusForField(sut, 'password', validationError)
-    Helper.testStatusForField(sut, 'password', validationError)
-    Helper.testStatusForField(sut, 'passwordConfirmation', validationError)
+    Helper.testStatusForField(sut, 'email', 'Campo obrigatório')
+    Helper.testStatusForField(sut, 'password', 'Campo obrigatório')
+    Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório')
+  })
+
+  test('Should show name error if Validation fails', () => {
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
+    populateField(sut, 'name')
+    Helper.testStatusForField(sut, 'name', validationError)
   })
 })
