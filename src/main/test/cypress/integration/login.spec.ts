02898cb11026fcdc4c860bf4f2d1cb544d9f2893
Jonnie <jonnieallobos@hotmail.com>
2020-10-27 23:59:48 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -2,8 +2,15 @@ describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
+  // testa o estado inicial da tela de Login
   it('Shuld load with correct initial state', () => {
-    // pega qualquer elemento que tenha o atributo data-testid com o valor email-status
-    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatório')
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório') // pega qualquer elemento que tenha o atributo data-testid com o valor email-status
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants') // não tem elemento filho
   })
 })
